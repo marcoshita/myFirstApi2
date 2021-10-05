0e5bc9f6d9e6b7397265fda7dfe2c67b928c870b
marcoshita <marcos.hita@gracia.lasalle.cat>
2021-10-05 19:20:38 +0200

    4
    4

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -2,7 +2,7 @@ module.exports.helloworld = (req, res) => {
   let responseObj = {};
   const nombreAlumnos = ["Xavi, Jordi, Alvar, Beimar, Marcos, Pol, Roger, Raul, Ayoub, Gerard, Khadija, Alex, Alba, Eric, Miquel, Alejandro, Pau, Marc"];
   responseObj.alumnos = nombreAlumnos;
-  responseObj.contadorAlumnos = nombreAlumnos.length
+  responseObj.contadorAlumnos = nombreAlumnos.length;
   const responseStatus = 200;
   res.status(responseStatus).send(responseObj);
   }
