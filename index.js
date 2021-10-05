66e61e8225377032a605683c719f35976468aa55
marcoshita <marcos.hita@gracia.lasalle.cat>
2021-10-05 19:03:02 +0200

    2
    2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv');
 const myFirstController = require('./controllers/myFirstController');
 
 app.get('/', myFirstController.helloworld);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('My first API running!!!!');
   });
