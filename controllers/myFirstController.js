6647c5d7d8b256d604f2d709fc92921951a7afec
marcoshita <marcos.hita@gracia.lasalle.cat>
2021-10-05 19:29:14 +0200

    6
    6

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -1,6 +1,6 @@
 module.exports.helloworld = (req, res) => {
   let responseObj = {};
-  const nombreAlumnos = ["Xavi, Jordi, Alvar, Beimar, Marcos, Pol, Roger, Raul, Ayoub, Gerard, Khadija, Alex, Alba, Eric, Miquel, Alejandro, Pau, Marc"];
+  const nombreAlumnos = ["Xavi","Jordi","Alvar","Beimar","Marcos","Pol","Roger","Raul","Ayoub","Gerard","Khadija","Alex","Alba","Eric","Miquel","Alejandro","Pau","Marc"];
   responseObj.alumnos = nombreAlumnos;
   responseObj.contadorAlumnos = nombreAlumnos.length;
   const responseStatus = 200;
