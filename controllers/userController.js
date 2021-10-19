a7ae8b137f743329f1e1b40616d8b42b3575f04f
marcoshita <marcos.hita@gracia.lasalle.cat>
2021-10-19 19:15:46 +0200

    prueba id nombres
    a

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+// module.exports.list = () => {
+
+// };
+
+// module.exports.profile = () => {
+
+// };
+const users = [
+  {id: 1, name: 'john'}, 
+  {id: 2, name:'david'},
+  {id: 3, name: 'maria'}
+];
+
+const status = {
+  ok: 200,
+  notFound: 404,
+};
+
+module.exports = {
+  list: (req, res) => {
+    console.log(req.body)
+    res.status(200).send(users);
+  },
+
+  profile: (req, res) => {
+    const user = users.find((user) => user.id == req.params.userId);
+    if(user) {
+      res.status(status.ok).send(user);
+    }else{
+      const msg = {error: 'user ID not found'};
+      res.status(status.notFound).send(msg);
+    }
+    
+  },
+  }
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const myFirstController = require('./controllers/myFirstController');
 
 app.get('/', myFirstController.helloworld);
 
+app.use('/api/v1/user', require('./routes/userRoutes'));
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('My first API running!!!!');
   });
